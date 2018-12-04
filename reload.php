9e51f213dbf8bd08b83567d5e01e92f930982f72
Rohmat Dasuki <dasukirohmat@gmail.com>
2018-12-04 15:11:21 +0700

    as

--- reload.php
+++ reload.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+if (!isset($_SESSION['username'])){
+    header("Location:login.php");
+}
+
+?>
+<?php
 	$ketinggian_air = $_POST['ketinggian_air'];
 	
 	$conn = mysqli_connect('localhost', 'root', '', 'db_pdap');
